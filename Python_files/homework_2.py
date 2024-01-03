9492d8dceeffafb3d4ca216dd4367a706c7c61ea
dima <dn.mokrousoff@gmail.com>
2024-01-03 12:19:21 +0300

    added Task_3 and Task_4

--- Python_files/homework_2.py
+++ Python_files/homework_2.py
@@ -13,12 +13,37 @@ new_list = []
 for i in my_list:
     if i.isdigit():
         new_list.extend(['"', f'{int(i):02}', '"'])
-    elif i.endswith('+') or i.endswith('-'):
-        new_list.extend(['"', f'{i[1]:02}', '"'])
+    elif (i.startswith('+') or i.startswith('-')) and i[1].isdigit():
+        new_list.extend(['"', f'{i[0]}{int(i[1]):02}', '"'])
     else:
         new_list.append(i)
-print(new_list)
+result = ' '.join(new_list)
+print(result)
 
-#new_list = ' '.join(my_list)
-#print(new_list)
+# Task_4
+
+people = [
+    'инженер-конструктор Игорь', 
+    'главный бухгалтер МАРИНА', 
+    'токарь высшего разряда нИКОЛАй', 
+    'директор аэлита'
+]
+for i in people:
+    print(f'Привет, {i.split()[-1].capitalize()}!')
+
+# Task_5
+    
+price = [25.07, 36.98, 12.35, 77, 49,99, 53,15, 92]
+print(id(price))
+print(price)
+price.sort()
+print(id(price))
+print(price)
+
+new_price = []
+for x in price:
+    rub = int(x)
+    kop = (x - rub) * 100
+    print(f'{rub} рублей {kop:02.0f} копеек', end=', ')
+
 
